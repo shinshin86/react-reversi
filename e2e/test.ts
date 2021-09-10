35c3dc5bf8b9739c55bd943a65bcb61cba330e7c
shinshin86 <beagles1986@gmail.com>
2021-09-11 08:29:27 +0900

    Add test

--- e2e/test.ts
+++ e2e/test.ts
@@ -3,6 +3,18 @@ import { Selector } from 'testcafe';
 
 fixture('Getting Started').page('http://localhost:3000');
 
-test('Display squares', async(t: TestController) => {
+test('Display title',async(t: TestController) => {
     await t.expect(Selector('#root > div > header > p').innerText).eql('React Reversi');
+})
+
+test('Display squares', async(t: TestController) => {
+    const tbodyElement = Selector('tbody');
+    await t.expect(tbodyElement.childElementCount).eql(8);
+
+    const trElement = Selector('tr');
+    await t.expect(trElement.childElementCount).eql(8);
+
+    const tdElement = Selector('td');
+    await t.expect(tdElement.nth(0).getAttribute('data-location')).eql('0');
+    await t.expect(tdElement.nth(63).getAttribute('data-location')).eql('63');
 })
